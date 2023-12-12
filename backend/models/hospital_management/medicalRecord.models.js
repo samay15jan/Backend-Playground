f529683d091588c5ecd31829ab0efde4ed0f4799
samay15jan <samay15jan@gmail.com>
2023-12-12 11:43:50 +0530

    Learned Basics

--- backend/models/hospital_management/medicalRecord.models.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordSchema = mongoose.Schema({}, {timestamps: true})
-
-export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordSchema)
