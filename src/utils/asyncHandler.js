1ac112d5e8c007a80fff5882301725d481f7f798
samay15jan <samay15jan@gmail.com>
2023-12-14 01:41:02 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+// Wrapper for handling async 
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+    try {
+        await requestHandler(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500 ).json({
+            success : false,
+            message : err.message
+        })
+    }
+}
+
+export {asyncHandler}
